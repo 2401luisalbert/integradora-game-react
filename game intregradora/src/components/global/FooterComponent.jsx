5e6ecd0d168ba04b6e73027965ab24ddfc2a41a2
2401luisalbert <125489391+2401luisalbert@users.noreply.github.com>
2023-10-07 13:06:38 -0600

    Add video, modified footer

--- game intregradora/src/components/global/FooterComponent.jsx
+++ game intregradora/src/components/global/FooterComponent.jsx
@@ -1,7 +1,7 @@
 const FooterComponent = () => {
   return (
     <footer className="bg-dark text-light text-center py-3">
-      <div className="container">
+      <div className="container d-flex align-items-center justify-content-center mt-2">
         <p>&copy; 2023 Tu Sitio Web</p>
       </div>
     </footer>
--- game intregradora/src/views/Home.jsx
+++ game intregradora/src/views/Home.jsx
@@ -8,14 +8,14 @@ const Home = () => {
     <div>
       <HeaderComponent />
       <div className="container mt-5 align-items-center d-flex flex-column">
-        <p className={styles.text_world} >
+        <p className={styles.text_world}>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
           deleniti suscipit quod eum aliquid excepturi, iste rerum aspernatur
           mollitia! Error placeat deleniti debitis impedit illo cupiditate
           voluptatibus recusandae labore nostrum?
         </p>
         <div className={styles.grid_container}>
-        {dataHomeWorlds.map((world) => (
+          {dataHomeWorlds.map((world) => (
             <CardComponent
               img={world.img}
               title={world.title}
@@ -25,12 +25,19 @@ const Home = () => {
             />
           ))}
         </div>
-        <p className={styles.text_world} >
+        <p className={styles.text_world}>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
           deleniti suscipit quod eum aliquid excepturi, iste rerum aspernatur
           mollitia! Error placeat deleniti debitis impedit illo cupiditate
           voluptatibus recusandae labore nostrum?
         </p>
+        <div className={styles.iframe_container}>
+          <iframe
+            src="https://www.youtube.com/embed/nWMKBdf-CGo?si=q_aTYGBHH7rxCWjP"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
+        </div>
       </div>
     </div>
   );
