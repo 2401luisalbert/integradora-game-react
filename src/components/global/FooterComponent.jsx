3c4a67540670a73561e7c2a10c74c6fbf8e279df
2401luisalbert <125489391+2401luisalbert@users.noreply.github.com>
2023-10-19 09:23:26 -0600

    modified home

--- src/components/global/FooterComponent.jsx
+++ src/components/global/FooterComponent.jsx
@@ -2,7 +2,7 @@ const FooterComponent = () => {
   return (
     <footer className="bg-dark text-light text-center py-3">
       <div className="container d-flex align-items-center justify-content-center mt-2">
-        <p>&copy; 2023 Tu Sitio Web</p>
+        <p>&copy; 2023 Space Defender</p>
       </div>
     </footer>
   );
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -9,8 +9,9 @@ const Home = () => {
       <HeaderComponent />
       <div className="container mt-5">
 
-          <h2 className="title mb-5">Mundos</h2>
+        <h2 className="title my-5 py-2">Mundos</h2>
         <div className=" align-items-center d-flex flex-column">
+
           <p className={styles.text_world}>
             Explora la Luna, Marte y una estación espacial abandonada mientras
             recopilas pistas y muestras de vida alienígena. Encuentra esferas
@@ -29,25 +30,19 @@ const Home = () => {
               />
             ))}
           </div>
-          <p className={styles.text_world}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
-            deleniti suscipit quod eum aliquid excepturi, iste rerum aspernatur
-            mollitia! Error placeat deleniti debitis impedit illo cupiditate
-            voluptatibus recusandae labore nostrum?
-          </p>
         </div>
 
-          <h2 className="title mb-5">GamePlay</h2>
+
+        <h2 className="title my-5 py-2">GamePlay</h2>
         <div className={styles.iframe_container}>
           <iframe
-          src="https://www.youtube.com/embed/kfYEiTdsyas?si=vbFcZcWEkzL_r8vk"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            src="https://www.youtube.com/embed/kfYEiTdsyas?si=vbFcZcWEkzL_r8vk"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           ></iframe>
         </div>
       </div>
     </div>
-    
   );
 };
 
