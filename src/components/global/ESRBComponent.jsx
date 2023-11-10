da3bfe3f090930ec305aecf7b00e6cbcb39269b1
2401luisalbert <125489391+2401luisalbert@users.noreply.github.com>
2023-11-10 15:14:44 -0600

    Modified ESRB

--- src/components/global/ESRBComponent.jsx
+++ src/components/global/ESRBComponent.jsx
@@ -4,14 +4,13 @@ const ESRBComponent = () => {
   return (
     <div className={styles.container}>
       <div className={styles.esrb_rating}>
-        <div>Everyone</div>
+        <div>Everyone 10+</div>
         <div>
           <div>E</div>
         </div>
-        <div>content rated by</div>
         <div>esrb</div>
+        <div>10+</div>
       </div>
-      <p></p>
     </div>
   );
 };
